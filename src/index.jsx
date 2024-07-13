8e9a7db420406e5f52837d0de2b3d79ea96f037b
Faiez Fattah Wisnubrata <30339866+faiezfattah@users.noreply.github.com>
2024-07-13 13:04:29 +0700

    change <home> to <contact> for debugging purposes

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import Home from './components/Home/Home'
-import './index.css'
- 
-const root = createRoot(document.getElementById('root'));
-root.render(< Home />);
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Home from "./components/Home/Home";
+import "./index.css";
+
+// debugging purposes cuz me lazy
+import Contacts from "./components/Contacts/Contacts";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<Contacts />); // remember to change this
