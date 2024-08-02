d36dd4077b55c0ed6b0c78415de9b59e737d4658
Faiez Fattah Wisnubrata <30339866+faiezfattah@users.noreply.github.com>
2024-08-02 12:50:15 +0700

    change contact to home on index
    was for testing purposes cuz i couldnt get it to work on the navbar

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,5 @@ import { createRoot } from "react-dom/client";
 import Home from "./components/Home/Home";
 import "./index.css";
 
-// debugging purposes cuz me lazy
-import Contacts from "./components/Contacts/Contacts";
-
 const root = createRoot(document.getElementById("root"));
-root.render(<Contacts />); // remember to change this
+root.render(<Home />);
