4be776e27e18504cd5db12e9c134da2fba7308f3
MiftakhulAR <miftakhul.arigin@gmail.com>
2024-08-04 12:45:45 +0700

    Update Navigasi.jsx

--- src/components/Navbar/Navigasi.jsx
+++ src/components/Navbar/Navigasi.jsx
@@ -6,7 +6,7 @@ function Navigasi() {
     <ul className="flex gap-10 text-white font-semibold">
       <li><Link to="/" className='hover:text-yellow-400'>Home</Link></li>
       <li><Link to="/about" className='hover:text-yellow-400'>About</Link></li>
-      <li><a href="#divisi" className='hover:text-yellow-400'>Divisi</a></li>
+      <li><Link to="/divisi" className='hover:text-yellow-400'>Divisi</Link></li>
       <li><a href="#ContactUs" className='hover:text-yellow-400'>Contact Us</a></li>
     </ul>
   );
